9aee45c534dbb4f0bfbae93609e1616eb49e7a12
Hoenn <Evan@10.0.0.31>
2015-02-20 23:42:09 -0800

    This was most certainly a test

--- src/me/glassware/states/Menu.java
+++ src/me/glassware/states/Menu.java
@@ -19,7 +19,7 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
-
+//This is a commit test
 public class Menu extends GameState
 {	
 	private World world;
