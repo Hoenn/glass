909dc2ef1b16db2db32781e837abe3c1a2837776
ANDREW TRUETT < andrewtruett@aol.com>
2015-02-20 23:56:10 -0500

    Re-added B2DVars

---
+++ src/me/glassware/handlers/B2DVars.java
@@ -0,0 +1,15 @@
+package me.glassware.handlers;
+
+public class B2DVars
+{
+	//Pixels Per Meter
+	public static final float PPM = 100;
+	
+	//category bits
+	public static final short BIT_GROUND =2;
+	public static final short BIT_BOX =4;
+	public static final short BIT_BALL=8;
+	
+	
+
+}
