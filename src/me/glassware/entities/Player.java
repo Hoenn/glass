4852cb40d5ee8a45112c5a8003fc436a20c80605
Hoenn <skatevan1313@gmail.com>
2015-02-28 20:45:16 -0500

    Added player class, subclass of b2dsprite, holds string of path to animation images

--- src/me/glassware/entities/Player.java
+++ src/me/glassware/entities/Player.java
@@ -1,10 +1,37 @@
 package me.glassware.entities;
 
-public class Player extends Entity
+import me.glassware.main.Game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class Player extends B2DSprite
 {
-	public Player()
+	private int totalHealth;
+	private int currentHealth;
+	
+	public Player(Body body)
 	{
+		super(body);
 		
+		Texture tex = Game.res.getTexture("wizard");
+		TextureRegion[] sprites = TextureRegion.split(tex, 20, 20)[0];
+		setAnimation(sprites, 1/4f);
+		
+	}
+	public void setTotalHealth(int h)
+	{
+		totalHealth=h;
+	}
+	public int getHealth()
+	{
+		return currentHealth;
+	}
+	public void takeDamage(int dmg)
+	{
+		currentHealth-=dmg;
+		//TODO if dead
 	}
 }
 
