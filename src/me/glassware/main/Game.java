d41f8eef8b40a4583ea40e8504d7228bb35e302f
Hoenn <skatevan1313@gmail.com>
2015-03-03 20:54:59 -0500

    Added a boolean var showFPS, draws the current FPS to the hudcam in bottom left corner

--- src/me/glassware/main/Game.java
+++ src/me/glassware/main/Game.java
@@ -10,6 +10,7 @@ import me.glassware.handlers.GameStateManager;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 
@@ -24,13 +25,18 @@ public class Game implements ApplicationListener
 	
 	public static Array<String> itemList;
 	public static Random random;
+	
+	public static BitmapFont font;
+	
 	private float accum;
 	
 	private SpriteBatch sb;
 	private OrthographicCamera cam;
 	private OrthographicCamera hudCam;
-	
+		
 	private GameStateManager gsm;	
+	
+	private boolean showFPS=false;
 	
 	public static Content res;
 	
@@ -46,7 +52,7 @@ public class Game implements ApplicationListener
 		res.loadTexture("res/images/potion.png", "potion");
 		
 		random = new Random();
-		
+
 		itemList = new Array<String>();
 		itemList.add("sword");
 		itemList.add("potion");
@@ -57,6 +63,8 @@ public class Game implements ApplicationListener
 		cam.setToOrtho(false, V_WIDTH, V_HEIGHT);
 		hudCam = new OrthographicCamera();
 		hudCam.setToOrtho(false, V_WIDTH, V_HEIGHT);
+		
+		font=new BitmapFont();
 		
 		gsm = new GameStateManager(this);
 
@@ -78,7 +86,6 @@ public class Game implements ApplicationListener
 	}
 	public void render()
 	{
-
 		accum+= Gdx.graphics.getDeltaTime();
 		while(accum>=STEP)
 		{
@@ -87,8 +94,13 @@ public class Game implements ApplicationListener
 			gsm.render();
 			GameInput.update();
 		}
-
-		
+		if(showFPS)
+		{
+			sb.begin();
+			sb.setProjectionMatrix(hudCam.combined);
+			font.draw(sb, Integer.toString(Gdx.graphics.getFramesPerSecond()), 10, 15);
+			sb.end();
+		}
 	}
 	public void dispose()
 	{
